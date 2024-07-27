1c0eee71232ff820e1788a564a0ec628a164fc2f
Clayton79 <claytonxavier21s@gmail.com>
2024-07-27 17:08:47 -0300

    commit

--- Fundamentos/math.js
+++ Fundamentos/math.js
@@ -1,5 +1,7 @@
+//O método Math.pow calcula a base elevada ao expoente.
+
 const raio = 5.6;
 const area = Math.PI * Math.pow (raio, 2);
 
 console.log(area);
-console.log(typeof Math);
+console.log(typeof Math); 
