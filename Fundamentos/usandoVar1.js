0fc39dd6e9f1ae4d629e09e45fc34035248d97ee
Clayton79 <claytonxavier21s@gmail.com>
2024-07-29 16:30:07 -0300

    commit

---
+++ Fundamentos/usandoVar1.js
@@ -0,0 +1,16 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???';
+                console.log = (sera);
+            }
+        }
+    }
+}
+
+console.log(sera);
+
+function teste () {
+    var local = 123;
+};
