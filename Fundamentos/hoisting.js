534d048df5bd1e308a44764f64ca1724285304c2
Clayton79 <claytonxavier21s@gmail.com>
2024-07-30 14:29:58 -0300

    commit

---
+++ Fundamentos/hoisting.js
@@ -0,0 +1,7 @@
+console.log("a =",a)
+var a = 2  // Usando a variavel 'var' ela joga o código pra cima
+console.log("a = ",a)
+
+console.log("b =", b)
+let b = 2 // o let não tem essa função de levar código pra cima
+console.log("b =",b)
