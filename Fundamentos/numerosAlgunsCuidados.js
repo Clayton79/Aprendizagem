04600e7a54fb50a3c8ed344cb6b78756aa9ae241
Clayton79 <claytonxavier21s@gmail.com>
2024-08-01 16:05:36 -0300

    commit

--- Fundamentos/numerosAlgunsCuidados.js
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -17,7 +17,7 @@ console.log("Show!" * 2); // Exibe NaN (Not-a-Number), pois a string "Show!" nã
 console.log(0.1 + 0.7); // Exibe 0.7999999999999999 devido a imprecisões na representação de números de ponto flutuante em JavaScript
 
 // Tentativa de chamar toString() diretamente em um número (sem parênteses)
-console.log(10.toString()); // Erro! Sintaxe inválida. O correto é usar parênteses ou envolver o número em parênteses
+//console.log(10.toString()); // Erro! Sintaxe inválida. O correto é usar parênteses ou envolver o número em parênteses
 
 // Correção da chamada de toFixed(2) em um número com ponto flutuante
 console.log((10.345).toFixed(2)); // Exibe '10.35', que é a representação da variável com duas casas decimais
