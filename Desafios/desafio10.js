5099e81e03d0423aadf76b716cdcac8d45d30a5c
Clayton79 <claytonxavier21s@gmail.com>
2024-08-07 19:12:12 -0300

    commit

---
+++ Desafios/desafio10.js
@@ -0,0 +1,11 @@
+function verificar (numero) {
+    if (numero % 3 === 0 ) {
+        return  true
+    } else {
+        return false
+    }
+}
+
+
+console.log(verificar(9))
+console.log(verificar(10))
---
+++ Desafios/desafio24.js
@@ -0,0 +1,8 @@
+const falar  = "hello world"
+let loop = 0
+
+
+while (loop < 11) {
+    console.log(falar)
+    loop++
+}
