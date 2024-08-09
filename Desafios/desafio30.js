fd97837c51cd441f4830c5d36dbc9844f4e268d5
Clayton79 <claytonxavier21s@gmail.com>
2024-08-09 19:28:38 -0300

    commit

--- Desafios/desafio30.js
+++
@@ -1,7 +0,0 @@
-function Progressao (pontuacao1,pontuacao2,pontuacao3) {
-        pontuacao1 = 10
-        pontuacao2 = 20 
-        pontuacao3 = 30
-        return [3,7,10]
-}
-console.log(Progressao())
