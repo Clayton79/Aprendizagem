61b0d07773d8ff3403bab801804d01b18fdf765c
Clayton79 <claytonxavier21s@gmail.com>
2024-08-12 18:39:31 -0300

    commit

---
+++ Desafios/desafio34.js
@@ -0,0 +1,7 @@
+function Palavras (String1, String2, Juntos, verdade ) {
+    String1 =  "C4stelo"
+    String2 =  "Sh0ping"
+    return  Juntos = String1 + String2
+}
+
+console.log(Palavras(true))
