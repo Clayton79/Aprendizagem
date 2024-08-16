7f265855925bcade4c544b2fed8d6bcc0319cbb5
Clayton79 <claytonxavier21s@gmail.com>
2024-08-15 21:04:11 -0300

    commit

---
+++ Desafios/desafio52.js
@@ -0,0 +1,13 @@
+let a = 33
+let b = 92
+let c = 45
+
+let resultado =  a + (b % c)
+resultado = (a == b) &&  (b <=c ) 
+resultado = (a && b) || c
+
+b -= c
+a += b
+resultado =  a
+
+console.log(resultado)
