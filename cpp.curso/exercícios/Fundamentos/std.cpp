92da0f6746dc962a90569fe61415d7603f0d3b32
Clayton79 <claytonxavier21s@gmail.com>
2024-09-10 19:02:09 -0300

    commit

---
+++ cpp.curso/exercícios/Fundamentos/std.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // scope resolution operator
+    std::cout << "Good Morning" << std::endl;
+    cout << "Good Morning" << endl;
+
+    return 0;
+}
