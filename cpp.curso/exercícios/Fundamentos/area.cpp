bfd3b04011caf2ec58eba26279e1fd77cb6031d2
Clayton79 <claytonxavier21s@gmail.com>
2024-09-10 21:04:39 -0300

    commit

---
+++ cpp.curso/exercícios/Fundamentos/area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    const double PI = 3.1415;
+    double radius;
+
+    cout << "Enter radius ";
+    cin >> radius;
+    
+    double area = PI * radius * radius;
+    cout << "Area is: " << area ;
+
+    return 0;
+}
