9ec22505e77882aef8d35fa0ffa3e79d404e1ccb
Clayton79 <claytonxavier21s@gmail.com>
2024-09-17 17:32:55 -0300

    commit

---
+++ cpp.curso/exercícios/Fundamentos/unary.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int x = 2;
+    int y = 1;
+
+    x++; // postfix
+    ++y; // prefix
+    printf("%d %d\n", x, y);
+
+    --x; // postfix
+    y--; // prefix
+    printf("%d %d\n", x, y);
+
+    // x = 2 and y = 1 
+    printf("%d", x-- == ++y);
+    // x = 1 and y = 2
+    
+    
+
+    return 0;
+}
