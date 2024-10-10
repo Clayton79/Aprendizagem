30cbb5ac4d4205a93bf2e590e53763e737cd6f67
Clayton79 <claytonxavier21s@gmail.com>
2024-10-10 01:35:54 -0300

    commit

---
+++ Objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL !!!
+
+// Procedural
+processamento(valor1,valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() // foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstração
+// 2. enpasulamento
+// 3. herança (prototype)
+// 4. polimofismo
